296595ed390df61130f13af4c1a2df0da7469bd6
Ecanno8 <ethancannon92@gmail.com>
2024-07-21 19:19:17 -0700

    update after mentor meeting

--- testAng/src/app/user-login-form/user-login-form.component.ts
+++ testAng/src/app/user-login-form/user-login-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { FetchApiDataService } from '../fetch-api-data.service';
 import { MatDialogRef } from '@angular/material/dialog';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form.component';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
